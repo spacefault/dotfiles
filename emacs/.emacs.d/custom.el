dfaec8b582facd819f213590bdfeabdd4f43b599
spacefault <74156492+spacefault@users.noreply.github.com>
2026-05-14 11:25:45 -0600

    add custom.el and early-init.el

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f9d423fcd4581f368b08c720f04d206ee80b37bfb314fa37e279f554b6f415e9"
+     default))
+ '(package-selected-packages '(catppuccin-theme magit nix-mode org-roam rust-mode)))
---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,11 @@
+;; early init
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq org-startup-indented t)
+(column-number-mode)
+(display-time-mode)
+(display-battery-mode)
+
+(add-to-list 'default-frame-alist
+             '(font . "Monaspace Radon-12"))
